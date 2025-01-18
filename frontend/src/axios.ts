533a1f89959128c3229fb7e618146a2a7d77eacd
AndresElacion <andreselacioniii@gmail.com>
2025-01-18 16:39:43 +0800

    created order components

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -3,7 +3,7 @@ axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
 const apiURL = axios.create({
-  baseURL: 'http://localhost:8000', // Your Laravel API URL
+  baseURL: 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
     "X-CSRF-TOKEN": document.head.querySelector('meta[name="csrf-token"]')?.getAttribute("content") || "",
